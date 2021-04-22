581dddb7f1083244234e71e70d07680d8810ddce
Tom-Mertens <tom.mertens.97@gmail.com>
2021-04-22 14:16:41 +0200

    add_files

--- FPD_LOF.py
+++ FPD_LOF.py
@@ -4,6 +4,10 @@ import Packages
 # All processing functions are in Packages.py, this file only executes them and saves the results to a pickle file
 # this pickle file holds the output of the preprocessing, such as the DF with LOF scores.
 # From there, I used the Jupyter notebook called "LSTM.ipynb" to test the results, as it is easier with visualisations.
+# Make sure the dataset is in the data folder (you can download it from google drive)
+# In the config file you can further change the trajectories or designated folders.
+
+
 if __name__ == "__main__":
-    all_results = Packages.create_historical_outlier_dfs()
+    all_results = Packages.create_historical_outlier_dfs(data_folder)
     pickle.dump(all_results, open("./results/OBIS_results.pickle", "wb"))
